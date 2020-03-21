d96fe8f74c1a22606fdad029493fbba1f34bd73e
Andre Filliettaz <andrentaz@gmail.com>
2020-03-21 17:05:38 -0300

    Change tree and node interfaces

--- avl_tree.py
+++ avl_tree.py
@@ -1,159 +1,109 @@
 # -*- coding: utf-8 -*-
-from tree import Tree
-
-
-class AVLTree(Tree):
-    """An abstraction of Balanced AVL Tree"""
-
-    def __init__(self, key=None, parent=None):
-        super(AVLTree, self).__init__(key, parent)
-        self.height = 0
+class TreeNode(object):
+    """
+    Simple implementation of a Tree Node
+    """
+    def __init__(self, key):
+        super(TreeNode, self).__init__()
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
 
     def __repr__(self):
         return (
-            'AVLTree(key={}, '
-            'parent={}, '
+            'Node(key={}, '
             'left={}, '
             'right={}, '
             'height={})'
         ).format(
             self.key,
-            self.parent.key if self.parent else None,
             self.left.key if self.left else None,
             self.right.key if self.right else None,
-            self.calculate_height(),
+            self.height,
         )
 
-    def calculate_height(self):
-        """
-        Calculate the height of a tree getting the max height of a tree plus 1.
 
-        :return: tree height
+class AVLTree(object):
+    """
+    An abstraction of Balanced AVL Tree. This class is thought as a blueprint to
+    format the tree passed as root in its methods.
+
+    The AVLTree object itself has no root or nodes within it, but its methods
+    take a root to work with
+    """
+    class DuplicatedKeyError(Exception):
+        pass
+
+    def get_height(self, root):
         """
-        if self.is_leaf():
+        Get the height of the root
+
+        :return: integer with the height
+        """
+        if root is None:
             return 0
 
-        left_height = self.left.calculate_height() if self.left else 0
-        right_height = self.right.calculate_height() if self.right else 0
+        return root.height
 
-        self.height = max(left_height, right_height) + 1
-        return self.height
+    def get_balance(self, root):
+        if root is None:
+            return 0
 
-    def left_rotate(self):
+        return self.get_height(root.left) - self.get_height(root.right)
+
+    def left_rotate(self, root):
         """
         Implements a left rotation in the tree.
 
-        :return: new root of the tree
+        :return: new root
         """
-        temp = self.right
-
-        self.right = temp.left
-        if self.right:
-            self.right.parent = self
-
-        temp.left = self
-        temp.parent = self.parent
-        self.parent = temp
+        temp = root.right
+        root.right = temp.left
+        temp.left = root
 
         return temp
 
-    def right_rotate(self):
+    def right_rotate(self, root):
         """
         Implements a right rotation in the tree.
 
         :return: new root of the tree
         """
-        temp = self.left
-
-        self.left = temp.right
-        if self.left:
-            self.left.parent = self
-
-        temp.right = self
-        temp.parent = self.parent
-        self.parent = temp
+        temp = root.left
+        root.left = temp.right
+        temp.right = root
 
         return temp
 
-    def left_right_rotate(self):
+    def left_right_rotate(self, root):
         """
         Implements a left-right rotation in the tree using the left and right
         basic rotations.
 
         :return: new root of the tree
         """
-        self.left = self.left.left_rotate()
-        return self.right_rotate()
+        root.left = self.left_rotate(root.left)
+        return self.right_rotate(root)
 
-    def right_left_rotate(self):
+    def right_left_rotate(self, root):
         """
         Implements a right-left rotation in the tree using the left and right
         basic rotations.
 
         :return: new root of the tree
         """
-        self.right = self.right.right_rotate()
-        return self.left_rotate()
+        root.right = self.right_rotate(root.right)
+        return self.left_rotate(root)
 
-    def rebalance(self):
-        """
-        Rebalance uses the 4 basic rotations to balance a tree and preserve the
-        AVL Tree balance properties.
-        Example:
-
-            8
-           /        6
-          6    =>  / \
-         /        4   8
-        4
-
-        :return: The method always return the new balanced root of the tree
-        """
-        l_height = self.left.calculate_height() + 1 if self.left else 0
-        r_height = self.right.calculate_height() + 1 if self.right else 0
-
-        if abs(l_height - r_height) > 1:
-            if l_height > r_height:
-                temp = self.left
-
-                pl_height = (
-                    temp.left.calculate_height() + 1
-                ) if temp.left else 0
-
-                pr_height = (
-                    temp.right.calculate_height() + 1
-                ) if temp.right else 0
-
-                if pl_height > pr_height:
-                    return self.right_rotate()
-                else:
-                    return self.left_right_rotate()
-            else:
-                temp = self.right
-
-                pl_height = (
-                    temp.left.calculate_height() + 1
-                ) if temp.left else 0
-
-                pr_height = (
-                    temp.right.calculate_height() + 1
-                ) if temp.right else 0
-
-                if pl_height > pr_height:
-                    return self.right_left_rotate()
-                else:
-                    return self.left_rotate()
-
-        return self
-
-    def insert(self, key):
+    def insert(self, root, key):
         """
         Implements AVLTree insertion recursively. The insertion preserves the
         AVL Tree property, which means it can change the root to keep the tree
         balanced.
 
         Example:
-            root = root.insert(89)
+            root = tree.insert(root, 89)
 
         This method always returns the root of the tree, which means that to
         keep always the root reference it's required to use the returned value
@@ -165,27 +115,45 @@ class AVLTree(Tree):
         :param key: Key to be inserted in the tree
         :return self: New tree root
         """
+        if root is None:
+            return TreeNode(key)
+        elif root.key > key:
+            root.left = self.insert(root.left, key)
+        elif root.key < key:
+            root.right = self.insert(root.right, key)
+        else:
+            raise DuplicatedKeyError(
+                'DuplicatedKeyError: {} is alrefy in the AVL Tree'.format(key)
+            )
 
-        if not self.key:
-            self.key = key
-            return self
+        root.height = max(
+            self.get_height(root.left),
+            self.get_height(root.right),
+        ) + 1
 
-        if self.key > key:
-            if not self.left:
-                self.left = AVLTree(key, self)
-                return self
-            else:
-                self.left.insert(key)
-                return self.rebalance()
+        balance = self.get_balance(root)
 
-        if self.key < key:
-            if not self.right:
-                self.right = AVLTree(key, self)
-                return self
-            else:
-                self.right.insert(key)
-                return self.rebalance()
 
-        raise DuplicatedKeyError(
-            'DuplicatedKeyError: {} is already in the tree'.format(key)
-        )
+        if balance > 1 and key < root.left.key:
+            # left - left unbalance
+            return self.right_rotate(root)
+        elif balance > 1 and key > root.left.key:
+            # left - right unbalance
+            return self.left_right_rotate(root)
+        elif balance < -1 and key > root.right.key:
+            # right - right unbalance
+            return self.left_rotate(root)
+        elif balance < -1 and key < root.right.key:
+            # right - left unbalance
+            return self.right_left_rotate(root)
+
+        return root
+
+    def inorder(self, root):
+        if root is None:
+            return
+
+        self.inorder(root.left)
+        print(' {} '.format(root.key), end='')
+        self.inorder(root.right)
+        return
